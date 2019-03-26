5d543e8d8219987a8a321e83ba066a6e9044352e
xhs <chef@dark.kitchen>
2019-03-27 01:11:03 +0800

    support logging

--- src/main/groovy/App.groovy
+++ src/main/groovy/App.groovy
@@ -1,16 +1,20 @@
 package kitchen.dark.groovy.web
 
 import io.javalin.Javalin
+import org.slf4j.LoggerFactory
 
 class App {
+    def final private static logger = LoggerFactory.getLogger(App.class)
+
     static void main(String... args) {
         def app = Javalin.create()
                 .disableStartupBanner()
                 .contextPath(System.getenv('CONTEXT_ROOT') ?: '/')
                 .disableDynamicGzip()
-        app.get("/", { ctx -> ctx.json([payload: 'Hello.']) })
+        app.get("/hello", { ctx -> ctx.json([payload: 'Hello.']) })
 
         def port = System.getenv('PORT') ?: 8888
         app.start(port as Integer)
+        logger.info("listening on port $port")
     }
 }
